d495525d0ab5d0697e87428d91251f735f7102e0
robj <robert.jennings@gmail.com>
2018-08-09 18:37:36 +0100

    initial account serializer

---
+++ app/serializers/account_serializer.rb
@@ -0,0 +1,13 @@
+class AccountSerializer < ActiveModel::Serializer
+ attribute :id
+ attribute :name
+ attribute :primary
+ attribute :type
+ attribute :currency
+ attribute :balance
+ attribute :created_at
+ attribute :updated_at
+ attribute :resource
+ attribute :resource_path
+ attribute :native_balance
+end
