1f6801a79b273099003ed2ca61b247b0dce88840
robj <robert.jennings@gmail.com>
2018-08-09 12:08:22 +0100

    basic versioned API plumbing from http://jes.al/2013/10/architecting-restful-rails-4-api/

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
+
     root :to => "sessions#new"
+
     resource :account
     resource :sessions
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+    namespace :api, defaults: {format: :json} do
+      scope module: :v1, constraints: ApiConstraints.new(version: 1, default: :true) do
+        resource :account
+      end
+    end
+
 end
