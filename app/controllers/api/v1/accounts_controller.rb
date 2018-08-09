0293876703eab596b4e7bcdf8ce8d60c270ade26
robj <robert.jennings@gmail.com>
2018-08-09 18:38:46 +0100

    account parse and return json

--- app/controllers/api/v1/accounts_controller.rb
+++ app/controllers/api/v1/accounts_controller.rb
@@ -1,5 +1,19 @@
 class Api::V1::AccountsController < Api::V1::BaseController
-    def show
-        render json: 'ok'
-    end
+
+  def show
+
+     unless (params.has_key?('api_key') && params.has_key?('api_secret'))
+        render json: {error: 'missing api credentials'}, status: :unprocessable_entity and return 
+     end 
+     
+     cap = CoinbaseAccountParser.new
+
+     if cap.get_account(params[:api_key],params[:api_secret])
+        render json: cap.account
+     else
+        render json: {error: cap.error}, status: :bad_request
+     end
+
+  end
+
 end
