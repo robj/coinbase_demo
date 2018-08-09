5147e691684839bff60a4ced2c6efc7253c8184e
robj <robert.jennings@gmail.com>
2018-08-09 18:39:06 +0100

    error handling: ensure required params are present

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,18 +2,22 @@ require 'coinbase/wallet'
 
 class AccountsController < ApplicationController
 
-    def show
+  def show
 
-         cap = CoinbaseAccountParser.new
-         
-         if cap.get_account(params[:api_key],params[:api_secret])
-            @account = cap.account
-            @error = nil
-         else
-            @account = nil
-            @error = cap.error
-         end
+     unless (params.has_key?('api_key') && params.has_key?('api_secret'))
+        render plain: 'missing api credentials', status: :unprocessable_entity and return 
+     end 
 
+    cap = CoinbaseAccountParser.new
+    
+    if cap.get_account(params[:api_key],params[:api_secret])
+       @account = cap.account
+       @error = nil
+    else
+       @account = nil
+       @error = cap.error
     end
+
+  end
 
 end
